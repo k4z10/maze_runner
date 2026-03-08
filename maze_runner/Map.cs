408cd697b90a441c918f32ed35ba9e71d49693c1
Kazimierz Krześniak <kazimierz.krzesniak06@gmail.com>
2026-03-08 13:34:57 +0100

    Added RandomTiles maze generator

--- maze_runner/Map.cs
+++ maze_runner/Map.cs
@@ -1,63 +1,66 @@
 using System.Text;
 
 namespace maze_runner;
-public abstract class Map(int sizeX, int sizeY)
+public abstract class Map(int rows, int cols)
 {
-    protected readonly Tile[,] Tiles = new Tile[sizeX, sizeY];
+    public readonly int Rows = rows;
+    public readonly int Cols = cols;
+    
+    protected readonly Tile[,] Tiles = new Tile[rows, cols];
     public abstract void GenerateMaze();
 
     private static readonly WallTile OutOfBounds = new WallTile();
     public Tile GetTile(int x, int y)
     {
-        if (x < 0 || x >= sizeX || y < 0 || y >= sizeY)
+        if (x < 0 || x >= Rows || y < 0 || y >= Cols)
         {
             return OutOfBounds;
         }
         return Tiles[x, y];
     }
 
+    private string? _mapString;
     public override string ToString()
     {
+        if (_mapString != null)
+            return _mapString;
         StringBuilder sb = new();
-        for (int i = 0; i < sizeX; i++)
+        for (int i = 0; i < Rows; i++)
         {
-            for (int j = 0; j < sizeY; j++)
+            for (int j = 0; j < Cols; j++)
             {
                 sb.Append(Tiles[i,j].GetTileSymbol());
             }
             sb.AppendLine();
         }
-        
-        return sb.ToString();
+        _mapString = sb.ToString();
+        return _mapString;
     }
 }
 
-public class EmptyMaze(int sizeX, int sizeY) : Map(sizeX, sizeY)
+public class EmptyMaze(int rows, int cols) : Map(rows, cols)
 {
     public override void GenerateMaze()
     {
-        for (int i = 0; i < sizeX; i++)
+        for (int i = 0; i < Rows; i++)
         {
-            for (int j = 0; j < sizeY; j++)
+            for (int j = 0; j < Cols; j++)
             {
-                if (j == 0 || i == 0 || i == sizeX - 1 || j == sizeY - 1)
-                    Tiles[i,j] = new WallTile();
-                else
-                    Tiles[i,j] = new FloorTile();
+                Tiles[i,j] = new FloorTile();
             }
         }
     }
 }
 
-public class FromFileMaze(int sizeX, int sizeY, string fileName) : Map(sizeX, sizeY)
+public class FromFileMaze(int rows, int cols, string fileName) : Map(rows, cols)
 {
     public override void GenerateMaze()
     {
         using StreamReader reader = new(fileName);
-        for (int i = 0; i < sizeX; i++)
+        for (int i = 0; i < Rows; i++)
         {
             var line = reader.ReadLine();
-            for (int j = 0; j < sizeY; j++)
+            for (int j = 0; j < Cols; j++)
             {
                 if (line == null || line.Length <= j || line[j] == '#' )
                     Tiles[i, j] = new WallTile();
@@ -66,6 +69,16 @@ public class FromFileMaze(int sizeX, int sizeY, string fileName) : Map(sizeX, si
             }
         }
     }
+}
+
+public class RandomTiles(int rows, int cols) : Map(rows, cols)
+{
+    public override void GenerateMaze()
+    {
+        for (int i = 0; i < Rows; i++)
+            for (int j = 0; j < Cols; j++)
+                Tiles[i,j] = Random.Shared.Next(0, 2) == 1 ? new FloorTile() : new WallTile();
+    } 
 }
 
 public abstract class Tile
