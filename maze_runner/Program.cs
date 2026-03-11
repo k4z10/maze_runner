e2d14cbf6f971cc2bc4b8e155815046e80c28658
Kazimierz Krześniak <kazimierz.krzesniak06@gmail.com>
2026-03-11 11:58:24 +0100

    Added demo

--- maze_runner/Program.cs
+++ maze_runner/Program.cs
@@ -5,8 +5,8 @@ static class Program
     static void Main(string[] args)
     {
         // EmptyMaze map = new EmptyMaze(20, 40);
-         // var map = new FromFileMaze(20, 40, "maze1.txt"); 
-        var map = new EmptyMaze(20, 40);
+         var map = new FromFileMaze(20, 40, "maze1.txt"); 
+        // var map = new EmptyMaze(20, 40);
         // var map = new RandomTiles(20, 40);
         map.GenerateMaze();
 
@@ -14,14 +14,15 @@ static class Program
 
         // Dodawanie Item do Planszy
         // TODO: delete
-        map.Tiles[10,10].AddItem(new Coin(10));
-        map.Tiles[10,10].AddItem(new Gold(10));
-        map.Tiles[11,11].AddItem(new Feather());
-        map.Tiles[10,11].AddItem(new Stick());
-        map.Tiles[9,11].AddItem(new Bottle());
+        map.Tiles[1,1].AddItem(new Coin(10));
+        map.Tiles[1,2].AddItem(new Gold(10));
+        map.Tiles[0,1].AddItem(new Feather());
+        map.Tiles[0,2].AddItem(new Stick());
+        map.Tiles[0,3].AddItem(new Bottle());
         for (var i = 0; i < 20; i++)
         {
-            map.Tiles[3,3].AddItem(new Knife());
+            Item[] potential = [new Knife(), new Sword(), new LongSword(), new Coin(1), new Gold(1), new Feather(), new Bottle(), new Stick()];
+            map.Tiles[3,3].AddItem(potential[Random.Shared.Next(0, potential.Length)]);
         }
         map.Tiles[1,1].AddItem(new Sword());
         map.Tiles[1,2].AddItem(new Knife());
